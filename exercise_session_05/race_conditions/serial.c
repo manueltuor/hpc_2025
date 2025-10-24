956ce3d769980aab89ae60b9305cd40c12f670a1
Manuel Tuor <mtuor@eiger-ln002.cscs.ch>
2025-10-24 12:29:57 +0200

    Exercise 5.3

--- exercise_session_05/race_conditions/serial.c
+++ exercise_session_05/race_conditions/serial.c
@@ -54,12 +54,24 @@ int main(int argc, char *argv[]){
 
     // first loop
     int maxval = 0;
-    for (int i=0;i<num_size;i++) if (numbers[i] > maxval) maxval = numbers[i];
+    # pragma omp parallel for
+    for (int i=0;i<num_size;i++) {
+	 #pragma omp critical
+	 if (numbers[i] > maxval) {
+		 maxval = numbers[i];
+	}
+    }
     printf("max number in file: %d\n",maxval);	
 
     // second loop
     int num_n0 = 0;
-    for (int i=0;i<num_size;i++) if (numbers[i] == 0) num_n0++;
+    # pragma omp parallel for
+    for (int i=0;i<num_size;i++) { 
+	if (numbers[i] == 0) { 
+		#pragma omp atomic
+		num_n0++;
+	}
+    }
     printf("number of 0s in file: %d\n",num_n0);  
     printf("true number of 0s in file: %d\n",true_n0);  
 

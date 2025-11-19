a5c2f0d98185a207497bb35be700cf31eb5de689
Manuel Tuor <mtuor@daint-ln004.cscs.ch>
2025-11-19 15:45:23 +0100

    Exercise 9.2

--- exercise_session_09/basics/blur_openacc.cpp
+++ exercise_session_09/basics/blur_openacc.cpp
@@ -7,7 +7,7 @@
 #include "util.h"
 
 #ifdef _OPENACC
-    // TODO: declare routine accordingly so as to be called from the GPU
+    #pragma acc routine seq
 #endif
 double blur(int pos, const double *u)
 {
@@ -39,7 +39,10 @@ void blur_twice_host(double *in , double *out , int n, int nsteps)
             out[i] = blur(i, buffer);
         }
 
+        double *tmp = in;
         in = out;
+        out = tmp;
+
     }
 
     free(buffer);
@@ -50,17 +53,25 @@ void blur_twice_gpu_naive(double *in , double *out , int n, int nsteps)
     double *buffer = malloc_host<double>(n);
 
     for (auto istep = 0; istep < nsteps; ++istep) {
-        // TODO: offload this loop to the GPU
-        for (auto i = 1; i < n-1; ++i) {
-            buffer[i] = blur(i, in);
+        #pragma acc data copyin(in[0:n]) copy(buffer[0:n])
+        {
+            #pragma acc kernels loop independent
+            for (auto i = 1; i < n-1; ++i) {
+                buffer[i] = blur(i, in);
+            }
         }
 
-        // TODO: offload this loop to the GPU
-        for (auto i = 2; i < n-2; ++i) {
-            out[i] = blur(i, buffer);
+        #pragma acc data copyin(buffer[0:n]) copy(out[0:n])
+        {
+            #pragma acc kernels loop independent
+            for (auto i = 2; i < n-2; ++i) {
+                out[i] = blur(i, buffer);
+            }
         }
 
+        double *tmp = in;
         in = out;
+        out = tmp;
     }
 
     free(buffer);
@@ -70,31 +81,35 @@ void blur_twice_gpu_nocopies(double *in , double *out , int n, int nsteps)
 {
     double *buffer = malloc_host<double>(n);
 
-    // TODO: move the data needed by the algorithm to the GPU
+    #pragma acc data copy(in[0:n], out[0:n]) create(buffer[0:n])
     {
+        double *u_in_dev = in;
+        double *u_out_dev = out;
+        double *d_buffer = buffer;
+
         for (auto istep = 0; istep < nsteps; ++istep) {
-            // TODO: offload this loop to the GPU
+            #pragma acc kernels loop independent present(u_in_dev, d_buffer)
             for (auto i = 1; i < n-1; ++i) {
-                buffer[i] = blur(i, in);
+                d_buffer[i] = blur(i, u_in_dev);
             }
 
-            // TODO: offload this loop to the GPU
+            #pragma acc kernels loop independent present(d_buffer, u_out_dev)
             for (auto i = 2; i < n-2; ++i) {
-                out[i] = blur(i, buffer);
+                u_out_dev[i] = blur(i, d_buffer);
             }
 
-            // TODO: offload this loop to the GPU; can you try just the pointer assignment?
-            for (auto i = 0; i < n; ++i) {
-                in[i] = out[i];
-            }
+            double *d_tmp = u_in_dev;
+            u_in_dev = u_out_dev;
+            u_out_dev = d_tmp;
         }
+
     }
 
     free(buffer);
 }
 
 int main(int argc, char** argv) {
-    size_t pow    = read_arg(argc, argv, 1, 20);
+    size_t pow     = read_arg(argc, argv, 1, 20);
     size_t nsteps = read_arg(argc, argv, 2, 100);
     size_t n = (1 << pow) + 4;
 
@@ -108,7 +123,6 @@ int main(int argc, char** argv) {
     auto x0 = malloc_host<double>(n, 0.);
     auto x1 = malloc_host<double>(n, 0.);
 
-    // set boundary conditions to 1
     x0[0]   = x1[0]   = 1.0;
     x0[1]   = x1[1]   = 1.0;
     x0[n-2] = x1[n-2] = 1.0;
@@ -123,14 +137,18 @@ int main(int argc, char** argv) {
     auto time_host = get_time() - tstart_host;
 
     auto tstart = get_time();
-    blur_twice_gpu_naive(x0, x1, n, nsteps);
+    blur_twice_gpu_nocopies(x0, x1, n, nsteps);
+    // blur_twice_gpu_naive(x0, x1, n, nsteps);
     auto time = get_time() - tstart;
 
     auto validate = true;
     for (auto i = 0; i < n; ++i) {
-        if (std::abs(x1_orig[i] - x1[i]) > 1.e-6) {
+        double expected = (nsteps % 2 == 0) ? x0_orig[i] : x1_orig[i];
+        double found = (nsteps % 2 == 0) ? x0[i] : x1[i];
+        
+        if (std::abs(expected - found) > 1.e-6) {
             std::cout << "item " << i << " differs (expected, found): "
-                      << x1_orig[i] << " != " << x1[i] << "\n";
+                      << expected << " != " << found << "\n";
             validate = false;
             break;
         }

82a67df0b6353faefa5da7f5e33eaa2201cf6046
manueltuor <manuel_tuor@bluewin.ch>
2025-11-26 14:48:08 +0100

    Exercise 10.4

--- exercise_session_10/Exercises/convol/convol.cu
+++ exercise_session_10/Exercises/convol/convol.cu
@@ -4,62 +4,97 @@
 #define STOP 0
 #define START 1
 
-#define BLOCKSIZE 256
+extern "C" void chrono(int kind, float *time);
 
+__global__ void kconvol(float *gpu_a, float *gpu_b, int n) {
+    int i, j, l;
 
-extern "C" void chrono (int kind, float *time);
+    l = blockDim.x * blockIdx.x + threadIdx.x;
 
-__global__ void kconvol (float *gpu_a, float *gpu_b, int n) {
-  int i, j, l;
-  // TO DO : evaluate the global 1D index l of the current thread,
-  // using blockDim, blockIdx and threadIdx.
+    j = l / n;
+    i = l - j * n;
 
-  
-  // TO DO : evaluate global indeces of thread (i,j) from the index l
-  j = ...;
-  i = ...;
+    if ((i >= n) || (j >= n)) return;
 
-  if ((i >= n) || (j >= n)) return;
-  if ((i == 0) || (j == 0) || (i == n-1) || (j == n-1))  {
-    gpu_b[l] = gpu_a[l]; // edges are untouched
-  }
-  else
-    // TO DO : fill up the MISSING indices below
-    gpu_b[l]=(1./5.)*(gpu_a[l-n] + gpu_a[/*MISSING*/] + gpu_a[l] + gpu_a[l+1]+ gpu_a[/*MISSING*/]);
+    if ((i == 0) || (j == 0) || (i == n - 1) || (j == n - 1)) {
+        gpu_b[l] = gpu_a[l];
+    }
+    else {
+        gpu_b[l] = (1.0f / 5.0f) *
+                   (gpu_a[l - n] + gpu_a[l - 1] + gpu_a[l] +
+                    gpu_a[l + 1] + gpu_a[l + n]);
+    }
 }
 
-extern "C" void gpu_convol (float *a, float *b, int n) {
-  float *gpu_a;
-  float *gpu_b;
-  cudaError_t err;
-  float time;
-  
-  err = cudaMalloc (&gpu_a, n*n*sizeof(float));
-  if (err != 0) {
-    printf ("Error allocating gpu_a: %s\n", cudaGetErrorString (err));
-    exit (1);
-  }
-  err = cudaMalloc (&gpu_b, n*n*sizeof(float));
-  if (err != 0) {
-    printf ("Error allocating gpu_b: %s\n", cudaGetErrorString (err));
-    exit (1);
-  }
-  
-  cudaMemcpy (gpu_a, a, n*n*sizeof(float), cudaMemcpyHostToDevice);
-    
-  // NOTE : the chronometer below does not contemplate overhead of memory allocation and
-  // memory transfer.
-  chrono (START, &time);
-  // TO DO : the number of blocks is missing below in the kernel invocation
-  kconvol <<</*MISSING*/,BLOCKSIZE>>> (gpu_a, gpu_b, n);
-  err=cudaDeviceSynchronize ();
-  chrono (STOP, &time);
-  printf ("Convolution took  %f sec. on GPU\n", time);
-  cudaMemcpy (b, gpu_b, n*n*sizeof(float), cudaMemcpyDeviceToHost);
-  if (err != 0) {
-    printf ("%s\n", cudaGetErrorString (err));
-    exit (1);
-  }
-  cudaFree (gpu_a);
-  cudaFree (gpu_b);
+extern "C" void gpu_convol(float *a, float *b, int n) {
+    float *gpu_a;
+    float *gpu_b;
+    cudaError_t err;
+    size_t size = n * n * sizeof(float);
+
+    err = cudaMalloc(&gpu_a, size);
+    if (err != 0) { printf("Error allocating gpu_a: %s\n", cudaGetErrorString(err)); exit(1); }
+    err = cudaMalloc(&gpu_b, size);
+    if (err != 0) { printf("Error allocating gpu_b: %s\n", cudaGetErrorString(err)); exit(1); }
+
+    cudaMemcpy(gpu_a, a, size, cudaMemcpyHostToDevice);
+
+    printf("\n--- GPU Block Size Tuning ---\n");
+    int total_elements = n * n;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    int best_block_size = 0;
+    float best_time = 1e9f;
+
+    int blockSizes[] = {8, 16, 32, 64, 128, 256, 512, 1024};
+    int nBlockSizes = sizeof(blockSizes)/sizeof(blockSizes[0]);
+
+    for (int t = 0; t < nBlockSizes; t++) {
+        int block_size = blockSizes[t];
+        int numBlocks = (total_elements + block_size - 1)/block_size;
+        float elapsed_ms;
+
+        cudaEventRecord(start, 0);
+        kconvol<<<numBlocks, block_size>>>(gpu_a, gpu_b, n);
+        err = cudaDeviceSynchronize();
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_ms, start, stop);
+
+        if (err != 0) {
+            printf("Error during kernel for block size %d: %s\n", block_size, cudaGetErrorString(err));
+            exit(1);
+        }
+
+        printf("Block size %4d: Convolution took %f ms\n", block_size, elapsed_ms);
+
+        if (elapsed_ms < best_time) {
+            best_time = elapsed_ms;
+            best_block_size = block_size;
+        }
+    }
+
+    printf("Optimal Block Size: %d (Time: %f ms)\n", best_block_size, best_time);
+    printf("-----------------------------\n");
+
+    int final_num_blocks = (total_elements + best_block_size - 1)/best_block_size;
+    float final_time;
+    cudaEventRecord(start, 0);
+    kconvol<<<final_num_blocks, best_block_size>>>(gpu_a, gpu_b, n);
+    err = cudaDeviceSynchronize();
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&final_time, start, stop);
+
+    printf("Convolution took %f ms on GPU\n", final_time);
+
+    cudaMemcpy(b, gpu_b, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(gpu_a);
+    cudaFree(gpu_b);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 }

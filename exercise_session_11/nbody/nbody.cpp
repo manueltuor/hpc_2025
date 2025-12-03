6352313a887e5dff62807a620649a9014a21a7e4
manueltuor <manuel_tuor@bluewin.ch>
2025-12-03 20:59:22 +0100

    Exercise 11.3

--- exercise_session_11/nbody/nbody.cpp
+++ exercise_session_11/nbody/nbody.cpp
@@ -1,54 +1,69 @@
 #include <random>
 #include <vector>
+#include <cmath>
 using std::vector;
 
-struct particle {
-    float x, y, z;	// position
-    float vx, vy, vz;	// velocity
-    float ax, ay, az;	// acceleration
+struct particles {
+    vector<float> x, y, z;    // position
+    vector<float> vx, vy, vz; // velocity
+    vector<float> ax, ay, az; // acceleration
 };
-typedef vector<particle> particles;
 
-// Initial conditions
 void ic(particles &plist, int n) {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<float> dis(0.0, 1.0);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
-    plist.clear(); 	// Remove all particles
-    plist.reserve(n);	// Make room for "n" particle
-    for( auto i=0; i<n; ++i) {
-	particle p { dis(gen),dis(gen),dis(gen),0,0,0,0,0,0 };
-	plist.push_back(p);
+    plist.x.resize(n);
+    plist.y.resize(n);
+    plist.z.resize(n);
+    plist.vx.resize(n, 0.0f);
+    plist.vy.resize(n, 0.0f);
+    plist.vz.resize(n, 0.0f);
+    plist.ax.resize(n, 0.0f);
+    plist.ay.resize(n, 0.0f);
+    plist.az.resize(n, 0.0f);
+
+    for (int i = 0; i < n; ++i) {
+        plist.x[i]  = dis(gen);
+        plist.y[i]  = dis(gen);
+        plist.z[i]  = dis(gen);
     }
-
 }
 
 void forces(particles &plist) {
-    int n = plist.size();
-    for(int i=0; i<n; ++i) { 		// We want to calculate the force on all particles
-	float ax=0, ay=0, az=0;
-        for(int j=0; j<n; ++j) {			// Depends on all other particles
-	    if (i==j) continue; // Skip self interaction 
-	    auto dx = plist[j].x - plist[i].x;
-	    auto dy = plist[j].y - plist[i].y;
-	    auto dz = plist[j].z - plist[i].z;
-	    auto r = sqrtf(dx*dx + dy*dy + dz*dz);
-	    auto ir3 = 1 / (r*r*r);
-	    ax += dx * ir3;
-	    ay += dy * ir3;
-	    az += dz * ir3;
-	}
-	plist[i].ax = ax;
-	plist[i].ay = ay;
-	plist[i].az = az;
+    int n = plist.x.size();
+
+    for (int i = 0; i < n; ++i) {
+        float ax = 0.0f, ay = 0.0f, az = 0.0f;
+
+        for (int j = 0; j < n; ++j) {
+            if (i == j) continue;
+
+            float dx = plist.x[j] - plist.x[i];
+            float dy = plist.y[j] - plist.y[i];
+            float dz = plist.z[j] - plist.z[i];
+
+            float r = sqrtf(dx*dx + dy*dy + dz*dz);
+            float ir3 = 1.0f / (r * r * r);
+
+            ax += dx * ir3;
+            ay += dy * ir3;
+            az += dz * ir3;
+        }
+
+        plist.ax[i] = ax;
+        plist.ay[i] = ay;
+        plist.az[i] = az;
     }
 }
 
 int main(int argc, char *argv[]) {
-    int N=50'000;
+    int N = 50000;
+
     particles plist;
-    ic(plist,N);
+    ic(plist, N);
     forces(plist);
+
     return 0;
 }
